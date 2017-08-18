ffabdb213a6105f34d9a7f22ade74b73ad647c3d
jratcliff <jratcliff@nvidia.com>
2017-08-18 10:38:05 -0500

    Revising binaries creating ignore list

--- public/NvPhysXFramework.h
+++ public/NvPhysXFramework.h
@@ -1,6 +1,15 @@
 #ifndef NV_PHYSX_FRAMEWORK_H
 #define NV_PHYSX_FRAMEWORK_H
 
+// The PhysX framework is a DLL which provides a very simple interface to perform
+// physics simulations using the PhysX SDK.  It also contains bindings to do debug
+// visualizations of the current simulation using the NvRenderDebug remote visualization
+// API and/or PVD (the PhysX Visual Debugger)
+// This is not intended to be a full function physics API but rather a minimal simple
+// system for getting basic demos to work.  The intial version was written specifically 
+// to improve and test the V-HACD (voxelized hierarchical convex decomposition library) results.
+// @see: https://github.com/kmammou/v-hacd
+
 namespace NV_PHYSX_FRAMEWORK
 {
 
