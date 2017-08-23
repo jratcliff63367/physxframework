07b28a59054ed3b6c2d91a5962b5945bf1e6f318
jratcliff <jratcliff@nvidia.com>
2017-08-23 09:03:33 -0500

    Removing 32 bit build configurations; not really needed and they just take up extra space.

--- compiler/xpj/create_projects.cmd
+++ compiler/xpj/create_projects.cmd
@@ -2,11 +2,11 @@
 
 set XPJ="xpj4.exe"
 
-%XPJ% -v 1 -t VC14 -p WIN32 -x SimpleHelloWorld.xpj
+@rem %XPJ% -v 1 -t VC14 -p WIN32 -x SimpleHelloWorld.xpj
 %XPJ% -v 1 -t VC14 -p WIN64 -x SimpleHelloWorld.xpj
 
 cd ..
-cd vc14win32
+cd vc14win64
 
 goto cleanExit
 
